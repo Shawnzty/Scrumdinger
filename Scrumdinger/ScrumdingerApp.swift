83e8e7e98ec028819854d3c68c6f75f01a7408fc
Tianyi Zheng <zhengty0509@gmail.com>
2023-06-24 23:01:25 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Tianyi Zheng on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
